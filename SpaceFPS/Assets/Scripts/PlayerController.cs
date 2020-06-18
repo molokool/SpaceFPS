f180ea6ad26e362d4d08cd224e6fa24263889d2d
molokool <molokool@outlook.com>
2020-06-18 18:22:49 +0200

    Add movement for player
    - Create Player Object with camera
    - Add 2 scripts joined to the Player
    - Add some 3D Object for the Scene

---
+++ SpaceFPS/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5f;
+    [SerializeField]
+    private float lookSensitivity = 3f;
+
+    private PlayerMotor motor;
+
+    private void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    private void Update()
+    {
+        // On va calculer la vélocité du mouvement du joueur en un Vecteur 3D
+        float _xMov = Input.GetAxisRaw("Horizontal");
+        float _zMov = Input.GetAxisRaw("Vertical");
+
+        Vector3 _movHorizontal = transform.right * _xMov;
+        Vector3 _movVertical = transform.forward * _zMov;
+
+        Vector3 _velocity = (_movHorizontal + _movVertical).normalized * speed;
+
+        motor.Move(_velocity);
+
+        // On va calculer la rotation du joueur en un Vecteur 3D
+        float _yRot = Input.GetAxisRaw("Mouse X");
+
+        Vector3 _rotation = new Vector3(0, _yRot, 0) * lookSensitivity;
+
+        motor.Rotate(_rotation);
+
+        // On va calculer la rotation de la camera en un Vecteur 3D
+        float _xRot = Input.GetAxisRaw("Mouse Y");
+
+        Vector3 _cameraRotation = new Vector3(_xRot, 0, 0) * lookSensitivity;
+
+        motor.RotateCamera(_cameraRotation);
+    }
+}
